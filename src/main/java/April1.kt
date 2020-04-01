70fcda0c09a1c75e7ecb0f692f1cb99ba000ea93
Sullivan, Denzel W <Denzel.Sullivan@dcsg.com>
2020-04-01 08:42:56 -0400

    Completed Challenge - Single Number

---
+++ src/main/java/April1.kt
@@ -0,0 +1,35 @@
+/**
+ * Single Number
+ *
+ * Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+ *
+ * Note:
+ * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ **/
+
+fun main() {
+    val example1 = listOf(2,2,1).toIntArray()
+    val example2 = listOf(4,1,2,1,2).toIntArray()
+
+    val s1 = Solution().singleNumber(example1)
+    val s2 = Solution().singleNumber(example2)
+
+    println("Solutions: $s1 $s2")
+}
+
+class Solution {
+    fun singleNumber(nums: IntArray): Int {
+        val list = mutableListOf<Int>()
+        nums.forEach {
+            if(list.contains(it)){
+                list.remove(it)
+            } else {
+                list.add(it)
+            }
+        }
+
+        println("size: ${list.size}, value: ${list[0]}")
+
+        return list[0]
+    }
+}
